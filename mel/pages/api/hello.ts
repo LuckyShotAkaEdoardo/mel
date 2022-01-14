cf7cd7ef0fe7811e3818e303c78ed0abd0cb6533
edoardo <edoardo.laurentino@gmail.com>
2022-01-14 17:33:08 +0100

    push

--- mel/pages/api/hello.ts
+++ mel/pages/api/hello.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
+type utenti = {
   name: string
+  cognome: string
 }
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const { slug } = req.query as { slug: string };
+  res.send(slug);
 }
